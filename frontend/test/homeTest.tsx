5602c37d431c922025f817c739c7f08180e8f1ed
Manuel <manuelv0703@gmail.com>
2026-04-09 18:27:26 -0500

    test: uploaded shell script and set up vitest

---
+++ frontend/test/homeTest.tsx
@@ -0,0 +1,5 @@
+import {expect, test} from 'vitest'
+import '@testing-library/jest-dom/vitest'
+// import { render, screen } from '@testing-library/react'
+
+test("canary", () => expect(true).toBe(true))
