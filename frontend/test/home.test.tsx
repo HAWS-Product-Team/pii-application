f274ad20dafaeba42c8016a02ef6d978ab4ca36d
Manuel <manuelv0703@gmail.com>
2026-04-14 18:45:20 -0500

    test: Title name appears in home page passes

--- frontend/test/home.test.tsx
+++ frontend/test/home.test.tsx
@@ -1,5 +1,17 @@
 import {expect, test} from 'vitest'
 import '@testing-library/jest-dom/vitest'
-// import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import Homepage from '../src/pages/HomePage'
+import { MemoryRouter } from 'react-router-dom'
 
 test("canary", () => expect(true).toBe(true))
+
+test("Home Page Title Loads", () => {
+    render(
+        <MemoryRouter>
+            <Homepage/>
+        </MemoryRouter>
+)
+
+    expect(screen.getByText("Track Your Inflation, Not Theirs")).toBeInTheDocument()
+})
